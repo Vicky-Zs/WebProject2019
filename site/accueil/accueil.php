7af6e2b7b4dbd9030c9109f969b7946d68cefb14
Vicky Zweistein <a.zweistein@gmx.com>
2019-05-11 16:28:35 +0200

    Modif accueil

--- site/accueil/accueil.php
+++ site/accueil/accueil.php
@@ -6,7 +6,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<title>Accueil</title>
-	<link rel="stylesheet" type="text/css" href="namemmrz.css">
+	<link rel="stylesheet" type="text/css" href="../namemmrz.css">
 </head>
 <body>
 	<div id="bvn">Bienvenu.e <strong>pseudo_user</strong> dans <br>Name Memorizer !</div>
@@ -14,7 +14,7 @@ session_start();
 		<p>
 			Ce site propose un service de <tst>mémoriseur de noms</tst> à destination de tous les enseignants qui ne retiennent jamais les prénoms de leurs élèves...<br>
 			L'apprentissage et la mémorisation se feront sous forme d'un jeu afin de rendre la tâche plus ludique.
-		</p>		
+		</p>
 		<?php
 			$_SESSION['teacher_id']=2;
 		?>
