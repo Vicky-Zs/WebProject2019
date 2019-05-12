f591596595e5a4a9dcb895f6ae7a1151eed99318
Vicky Zweistein <a.zweistein@gmx.com>
2019-05-12 23:08:21 +0200

    Ajout rapport et présentation, et correction des coquilles

--- namememorizer/accueil.php
+++ namememorizer/accueil.php
@@ -6,14 +6,14 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<title>Accueil</title>
-	<script type="text/javascript" src="../namemmrz.js"></script>
-	<link rel="stylesheet" type="text/css" href="../namemmrz.css">
+	<script type="text/javascript" src="namemmrz.js"></script>
+	<link rel="stylesheet" type="text/css" href="namemmrz.css">
 </head>
 <body>
 	<div id="my_acc">
 	<p id="pseudo"><c6><?php echo $_SESSION['username'];?></c6></p>
-		<a href="..profile/gestion.php">Mon compte</a>
-		<a href="../memorizer/play.php">Jouer</a>
+		<a href="profile/gestion.php">Mon compte</a>
+		<a href="memorizer/play.php">Jouer</a>
 		<a href="logout.php">Se deconnecter</a>
 	</div>
 	<div id="bvn">Bienvenue <strong id="pseudo"><?php echo $_SESSION['username'];?></strong> dans <br>Name Memorizer !</div>
@@ -23,11 +23,11 @@ session_start();
 			L'apprentissage et la mémorisation se feront sous forme d'un jeu afin de rendre la tâche plus ludique.
 		</p>
 		<br>
-		<p>Consulte l'<his>historique</his> de tes parties <a href="../memorizer/history.php" class="hvr-icon-forward"><img src="../background/arrow_icon.png" class="hvr-icon"></a>
+		<p>Consulte l'<his>historique</his> de tes parties <a href="memorizer/history.php" class="hvr-icon-forward"><img src="background/arrow_icon.png" class="hvr-icon"></a>
 		<br>
 		<p>Durée de l'épreuve: environ 20 minutes</p></p>
 
-		<p id="play_button"><a href="../memorizer/play.php">Jouer</a></p>
+		<p id="play_button"><a href="memorizer/play.php">Jouer</a></p>
 	</div>
 <form action="logout.php"><input type='submit' value='Se deconnecter'></form>
 
